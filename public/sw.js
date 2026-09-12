08d8485ae34c103ab2f0bd0e604b06b958f35db1
teiqo <wokworkout@gmail.com>
2026-09-12 19:48:39 +0500

    fix bugs

--- public/sw.js
+++ public/sw.js
@@ -1,6 +1,6 @@
 const SCOPE = new URL(self.registration.scope).pathname;
 const CACHE_PREFIX = "sched:" + SCOPE + ":";
-const CACHE = CACHE_PREFIX + "2026-09-12-v61-cancelled-gray";
+const CACHE = CACHE_PREFIX + "2026-09-12-v62-cancelled-room-gray";
 const ASSETS = [
   "./",
   "./index.html",
